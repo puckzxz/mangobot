00cb43571e60847e7ac55eac88b9ad46e2ed173a
Chris Bockover <cbock1337@gmail.com>
2024-08-08 10:16:27 -0400

    Adjust how we encode and decode

--- src/coder.ts
+++ src/coder.ts
@@ -1,7 +1,7 @@
 export const encode = (data: any): string => {
-  return btoa(unescape(encodeURIComponent(JSON.stringify(data))));
+  return btoa(JSON.stringify(data));
 };
 
 export function decode<T>(data: string): T {
-  return JSON.parse(decodeURIComponent(escape(atob(data))));
+  return JSON.parse(atob(data));
 }
