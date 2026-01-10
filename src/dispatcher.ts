6428eb9e88fc168c7b9d47c6ad5b69cdc8e0e68f
Chris Bockover <cbock1337@gmail.com>
2026-01-10 17:46:29 -0500

    Log errors

--- src/dispatcher.ts
+++ src/dispatcher.ts
@@ -5,6 +5,16 @@ export async function dispatchToSidecar<T>(data: Task): Promise<T> {
   const encoded = encode(data);
   const child = Bun.spawn(["bun", "src/sidecar.ts", encoded]);
   const rawReturn = await new Response(child.stdout).text();
-  await child.exited;
-  return decode<T>(rawReturn);
+  const errorMessage = await new Response(child.stderr).text();
+  if (errorMessage) {
+    console.error(errorMessage);
+    return [] as T;
+  }
+  try {
+    await child.exited;
+    return decode<T>(rawReturn);
+  } catch (error) {
+    console.error(error);
+    return [] as T;
+  }
 }
